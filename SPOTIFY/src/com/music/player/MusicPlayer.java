206b2ba4c0a462a4eb35472051fe2ae40f9e8b81
LEE-PABLO <leepaoula01@gmail.com>
2026-01-16 16:13:51 +0900

    0116

--- SPOTIFY/src/com/music/player/MusicPlayer.java
+++ SPOTIFY/src/com/music/player/MusicPlayer.java
@@ -13,7 +13,61 @@ public class MusicPlayer {
 	public MusicPlayer(boolean change) {
 		if(change) {musicList=new LinkedList<Music>();}
 		else {musicList=new ArrayList<Music>();}}
-	public void removeMusic(int idx) {musicList.remove(idx-1);}
+	
+	public Connection removeMusic(Connection conn,int idx) throws Exception {
+		conn.setAutoCommit(false);
+		StringBuffer sb= new StringBuffer();
+		sb.append(" DELETE FROM MUSIC WHERE MUSIC_NO=? ");
+		PreparedStatement pstmt =conn.prepareStatement(sb.toString());
+		pstmt.setObject(1, idx);
+		pstmt.executeUpdate();
+		sb= new StringBuffer();
+		sb.append(" DELETE FROM MUSIC_DETAIL WHERE MUSIC_NO=? ");
+		pstmt =conn.prepareStatement(sb.toString());
+		pstmt.setObject(1, idx);
+		pstmt.executeUpdate();
+		return conn;}
+	
+	public Connection updateTitle(Connection conn, String title ,int idx) throws Exception {
+		conn.setAutoCommit(false);
+		StringBuffer sb= new StringBuffer();
+		sb.append(" UPDATE MUSIC SET Title=?WHERE MUSIC_NO=? ");
+		PreparedStatement pstmt =conn.prepareStatement(sb.toString());
+		pstmt.setObject(1, title);
+		pstmt.setObject(2, idx);
+		pstmt.executeUpdate();
+		return conn;}
+	
+	public Connection updateVocal(Connection conn, String vocal ,int idx) throws Exception {
+		conn.setAutoCommit(false);
+		StringBuffer sb= new StringBuffer();
+		sb.append(" UPDATE MUSIC SET VOCAL=? WHERE MUSIC_NO=? ");
+		PreparedStatement pstmt =conn.prepareStatement(sb.toString());
+		pstmt.setObject(1, vocal);
+		pstmt.setObject(2, idx);
+		pstmt.executeUpdate();
+		return conn;}
+	
+	public Connection updateReleasComp(Connection conn,String releasComp,int idx) throws Exception {
+		conn.setAutoCommit(false);
+		StringBuffer sb= new StringBuffer();
+		sb.append(" UPDATE MUSIC_DETAIL SET RELEASE_COMP=? WHERE MUSIC_NO=? ");
+		PreparedStatement pstmt =conn.prepareStatement(sb.toString());
+		pstmt.setObject(1, releasComp);
+		pstmt.setObject(2, idx);
+		pstmt.executeUpdate();
+		return conn;}
+	
+	public Connection updateReleasDate(Connection conn,String releasDate,int idx) throws Exception {
+		conn.setAutoCommit(false);
+		StringBuffer sb= new StringBuffer();
+		sb.append(" UPDATE MUSIC_DETAIL SET RELEASE_DATE=? WHERE MUSIC_NO=? ");
+		PreparedStatement pstmt =conn.prepareStatement(sb.toString());
+		pstmt.setObject(1, releasDate);
+		pstmt.setObject(2, idx);
+		pstmt.executeUpdate();
+		return conn;}
+	
 	public Connection addMusic(
 	Connection conn, String vocal,String title, String releasComp, String releasDate) 
 	throws Exception {
@@ -26,7 +80,6 @@ public class MusicPlayer {
 		pstmt.setObject(2, vocal);
 		pstmt.executeUpdate();
 
-
 		sb= new StringBuffer();
 		sb.append(" INSERT INTO MUSIC_DETAIL (MUSIC_DETAIL_NO, RELEASE_COMP, RELEASE_DATE, MUSIC_NO) ");
 		sb.append(" VALUES (SEQ_MUSIC_DETAIL.NEXTVAL, ?, ?, SEQ_MUSIC.CURRVAL) ");
@@ -35,6 +88,7 @@ public class MusicPlayer {
 		pstmt.setObject(2, releasDate);
 		pstmt.executeUpdate();
 		return conn;}
+	
 	public List<Music> getMusicList(Connection conn){
 		try {Statement stmt=conn.createStatement();
 		StringBuffer sb= new StringBuffer();
--- SPOTIFY/src/com/music/player/UserInterface.java
+++ SPOTIFY/src/com/music/player/UserInterface.java
@@ -18,23 +18,33 @@ public class UserInterface {
 	public void start() {while(true) {
 		System.out.println("==============================");
 		System.out.println("Welcome to Spotify");
-		System.out.println("Press 1 to update your list");
-		System.out.println("Press 2 to check your list");
+		System.out.println("1: Add song to your list");
+		System.out.println("2: Check your list");
+		System.out.println("3: Delete music from your list");
+		System.out.println("4: Update song infomation on your list");
+		System.out.print("Choice: ");
 		String menu = sc.nextLine();
 		if("1".equals(menu)) {addMusic();}
 		if("2".equals(menu)) {showMusic();}
+		if("3".equals(menu)) {deleteMusic();}
+		if("4".equals(menu)) {updateMusic();}
 		System.out.println();}}
+	
 	private void addMusic() {try{System.out.println("==============================");
-	System.out.println("Please insert singername");
+	System.out.print("Insert Artist: ");
 	String singer=sc.nextLine();
-	System.out.println("Please insert title");
+	System.out.println("==============================");
+	System.out.print("Insert title: ");
 	String title=sc.nextLine();
-	System.out.println("Please insert release company");
+	System.out.println("==============================");
+	System.out.print("Insert release company: ");
 	String releaseCom=sc.nextLine();
-	System.out.println("Please insert release date");
+	System.out.println("==============================");
+	System.out.print("Insert release date: ");
 	String releaseDate=sc.nextLine();
 	conn=mp.addMusic(conn,singer,title,releaseCom,releaseDate);
-	System.out.println("Good to go?(Y/N)");
+	System.out.println("==============================");
+	System.out.print("Good to go?(Y/N): ");
 	String answer=sc.nextLine();
 	if("y".equalsIgnoreCase(answer)) {conn.commit();}
 	if("n".equalsIgnoreCase(answer)) {conn.rollback();}}
@@ -52,4 +62,59 @@ public class UserInterface {
 				,music.getReleaseComp()
 				,music.getReleaseDate());
 		System.out.println();}}
+	
+	private void deleteMusic() {try{this.showMusic();
+	System.out.println("==============================");
+	System.out.print("Insert the music number you want to delete: ");
+	int answer=sc.nextInt();
+	sc.nextLine();
+	conn=mp.removeMusic(conn,answer);
+	System.out.println("==============================");
+	System.out.print("Good to go?(Y/N): ");
+	String answer1=sc.nextLine();
+	if("y".equalsIgnoreCase(answer1)) {conn.commit();}
+	if("n".equalsIgnoreCase(answer1)) {conn.rollback();}}
+	catch(Exception e) {e.printStackTrace();}
+	try{conn.rollback();}catch(Exception e1) {e1.printStackTrace();}}
+	
+	private void updateMusic() {try{this.showMusic();
+	System.out.println("==============================");
+	System.out.print("Insert the music number you want to update: ");
+	int answer=sc.nextInt();
+	sc.nextLine();
+	while(true){
+		System.out.println("==============================");
+		System.out.print("1: Update Title\n2: Update Artist Name\n3: Update Company Name\n4: Update Release Date\n5: Quit Update\n");
+		System.out.print("Choice: ");
+		int answer1=sc.nextInt();
+		sc.nextLine();
+		System.out.println("==============================");
+		if(1==answer1) {
+		System.out.print("Insert new Title: ");
+		String title=sc.nextLine();
+		conn=mp.updateTitle(conn,title,answer);}
+	else if(2==answer1) {
+		System.out.print("Insert new Artist name: ");
+		String vocal=sc.nextLine();
+		conn=mp.updateVocal(conn,vocal,answer);}
+	else if(3==answer1) {
+		System.out.print("Insert new Company name: ");
+		String comp=sc.nextLine();
+		conn=mp.updateReleasComp(conn,comp,answer);}
+	else if(4==answer1) {
+		System.out.print("Insert new Release Date: ");
+		String date=sc.nextLine();
+		conn=mp.updateReleasDate(conn,date,answer);}
+	else if(5==answer1) { break;}
+	else {System.out.println("Choose between 1 to 5!;");}}
+	System.out.println("==============================");
+	System.out.print("Good to go?(Y/N): ");
+	String answer2=sc.nextLine();
+	if("y".equalsIgnoreCase(answer2)) {conn.commit();}
+	if("n".equalsIgnoreCase(answer2)) {conn.rollback();}}
+	catch(Exception e) {e.printStackTrace();}
+	try{conn.rollback();}catch(Exception e1) {e1.printStackTrace();}}
 }
+	
+
+
